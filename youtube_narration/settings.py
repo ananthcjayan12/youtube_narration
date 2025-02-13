91fea0c5a60890e06373b52703a096c6e14f96f7
ananthuuu <ananthcjayan12@gmail.com>
2025-02-13 23:20:54 +0530

    fix issues

--- youtube_narration/settings.py
+++ youtube_narration/settings.py
@@ -81,9 +81,9 @@ WSGI_APPLICATION = "youtube_narration.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'data', 'youtube_narration', 'db.sqlite3'),
     }
 }
 
